f529b867aaa265c0151007c682dce5f0e98f71bf
Alexey <96946444+webdev-alexey@users.noreply.github.com>
2022-03-07 12:36:24 +0300

    add burger + music + slider + fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+// Бургер-меню
+
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+   navigation.classList.add('navigation_active')
+});
+
+navigationClose.addEventListener('click', () => {
+   navigation.classList.remove('navigation_active')
+});
+
+// Фоновая музыка
+
+const mute = document.querySelector('.mute__checkbox');
+const audio = new Audio('audio/waterTower.mp3');
+
+mute.addEventListener('change', () => {
+   if (mute.checked) {
+      audio.play();
+   } else {
+      audio.pause();
+   }
+});
+
+
+// Слайдеры для страницы фото и видео
 try {
    const sliderThumbs = new Swiper('.slider-thumbs', {
       spaceBetween: 20,
@@ -19,6 +48,21 @@ try {
 
    sliderThumbs.controller.control = sliderMain;
    sliderMain.controller.control = sliderThumbs;
+
+   const videos = document.querySelectorAll('video');
+   sliderMain.on('slideChange', () => {
+      for (let i = 0; i < videos.length; i++) {
+         videos[i].pause();
+      }
+   });
+
+   const pagination = document.querySelector('.pagination');
+   const paginationButton = document.querySelector('.pagination__arrow');
+
+   paginationButton.addEventListener('click', () => {
+      pagination.classList.toggle('pagination_active');
+   });
+
 } catch {
    console.log()
 }
